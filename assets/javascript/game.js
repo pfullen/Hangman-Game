0767ea614e1658c817e0c88fe5f8e35c6fcd7d4c
Sabrina Barr <sabrinabarr@Sabrinas-MBP.lan>
2017-11-15 20:16:20 -0500

    Created Psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+// Hangman-Game  js file
+
+
+ //- make score equal zero
+
+ var wins = 0;
+
+// -  Tell player to press any key
+
+// -   Select a word at random from an array  -->	select a random #  Then use that number to get word in the array 
+
+
+
+// keep word hidden  using    _ _ _ _ _ _  
+
+// -  Ask user to guess a letter 
+
+   
+
+ //-  If the letter is included in the word then reveal the hidden letter
+	
+ //-  If the letter is not included in the word then add a body part to the screen
+ //   if the number of incorrect guesses equals the total number of body parts then all the body parts are reveled and user player lost the game.
+
+//	keep looping until user wins or loses
+ 
+ //   - If the user guesses the complete word then show firework gif
+
+ //     - play we are the champions
+
+ //      Then show a picture of the word
+ 
+
+// If user loses play  Hangmans coming and you have very long....   
+
+
+//Ask user if they want to play again 
+
+//	if yes them reset variables and start over
+
+//	if no then tell user "Thanks for playing"
+
