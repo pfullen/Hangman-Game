f775aaafe10f60f4e92af18544974256ff8e0d15
Sabrina Barr <sabrinabarr@Sabrinas-MBP.lan>
2017-11-18 09:10:58 -0500

    Added js code to select word at random with Letters are hidden by img

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,71 @@
 // Hangman-Game  js file
+$(document).ready(function() {
+
+ //- init game.   Get word and set number of chances to word length plus 7
+var chancesRemaining = 7;
+var correctAnswers = 0;
+var words = ["jedi", "lukeskywalker", "lightsaber", "chewbacca", "darthvader", "stormtrooper", "bobafett", "darthmaul"];
+
+// get word at random from array above
+
+var word = words[Math.floor(Math.random() * words.length)];
 
 
- //- make score equal zero
 
- var wins = 0;
+// The code below converst the word which is a sting into an array of each of its chars	
+word = word.split("");
+
+console.log(word);
+
+
+
+$.each(word, function(index, value) {
+	$('#word').append("<div class='letter'>   <div id='hidden'> " + value + "</div> </div>" );            //           
+ });
+
+});
+
+// $.each(function(word, index) {
+//	console.log (index + " ");
+//});
+
+
+
+
+
+//document.onkeyup = function(event) {
+
+//	var userChoice = event.key;
+
+  //  var index = word.indexOf(userChoice);
+	//console.log(index);
+
+	
+
+//	if (userChoice.indexOf(word) === -1) {
+//		document.write ("You need to focus young Jedi.  Your guess was not correct");
+//		chancesRemaining --;
+//	} else {
+//		document.write("The force is strong with you.  Your guess was correct");
+	
+
+
+
+ //};
+
+
+// put the below code into a function called  result().     
+//if (chancesRemaining === 0 ) {
+//	document.write ('Hang man is coming.  You lost');
+//} else if (correctAnswers === word.length) {
+//	document.write ('May the force be with you.  You have avoided the carbon freeze')
+//} else {
+
+//};
+
 
 // -  Tell player to press any key
+
 
 // -   Select a word at random from an array  -->	select a random #  Then use that number to get word in the array 
 
@@ -40,3 +100,22 @@
 
 //	if no then tell user "Thanks for playing"
 
+
+
+
+//   Game  --->  calls functions
+
+//  startGame();     // initialize game   set all var to inital value 
+
+//  getWord();     // Selects a word at random from an array of words
+// userTurn();       // gets user guess   
+
+// doesGuessMatch();  // checks to see if the the user's gueess of letter is in the 
+
+// updateRemainingGuesses();    // loweres the remmaining guuesses if an incorrect answer is guessed
+
+// playMusic();    // plays music
+
+// showAnswer();   // Reveals all the hidden letters
+
+// 
